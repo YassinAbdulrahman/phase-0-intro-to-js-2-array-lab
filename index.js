88d0f80d55bda0a3373712a074323e1e509dc95d
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-01-02 19:15:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return `${cats.push(name)}`;
+}
+
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name) {
+    return `${cats.unshift(name)}`;
+}
+
+destructivelyPrependCat("Bob");
+
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+
+function destructivelyRemoveLastCat() {
+    return `${cats.pop()}`;
+}
+destructivelyRemoveLastCat()
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+function destructivelyRemoveFirstCat() {
+    return `${cats.shift()}`;
+}
+destructivelyRemoveFirstCat()
+
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+
+function appendCat(name) {
+    return `${cats.splice(3, 0, name)}`
+}
+
+appendCat('Broom');
+
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+
+function prependCat(name){
+    return `${cats.splice(0,0,name)}`
+}
+prependCat("Arnold");
+console.log(cats);
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+function removeLastCat(){
+    return `${cats.}`
+}
+
+
+
